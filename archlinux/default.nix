1130138ddad0b40a7ab5a60db268ca6be6d5d77b
SamLukeYes <samlukeyes123@gmail.com>
2022-09-05 00:54:41 +0800

    Add paru

--- archlinux/default.nix
+++ archlinux/default.nix
@@ -5,15 +5,24 @@
 
 with pkgs; rec {
   archlinux-keyring = callPackage ./archlinux-keyring { inherit rp; };
-  pacman = callPackage ./pacman { inherit rp; };
+
+  asp = callPackage ./asp { inherit rp; };
+
+  arch-install-scripts = callPackage "${
+    builtins.fetchTarball "${rp}https://github.com/SamLukeYes/nixpkgs/archive/arch-install-scripts.tar.gz"
+  }/pkgs/tools/misc/arch-install-scripts" {};
+
+  devtools = callPackage ./devtools { inherit rp; };
+
+  pacman = callPackage "${
+    builtins.fetchTarball "${rp}https://github.com/SamLukeYes/nixpkgs/archive/pacman.tar.gz"
+  }/pkgs/tools/package-management/pacman" {};
+
   pacman-gnupg = callPackage ./pacman/gnupg.nix {
-    inherit pacman;
     keyrings = [ archlinux-keyring ];
   };
-  arch-install-scripts = callPackage ./arch-install-scripts {
-    inherit rp pacman; 
-  };
-  devtools = callPackage ./devtools {
-    inherit rp pacman arch-install-scripts;
-  };
+
+  paru-unwrapped = callPackage ./paru/unwrapped.nix { inherit rp; };
+
+  paru = callPackage ./paru { inherit asp devtools paru-unwrapped; };
 }

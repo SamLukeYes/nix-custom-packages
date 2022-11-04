f5b9bc29a12ca9342cbc5283d043608a71c6b7da
SamLukeYes <samlukeyes123@gmail.com>
2022-11-04 23:07:11 +0800

    flake: fix up!!!

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "Experimental flake for CI testing";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: 
-  let system = "x86_64-linux"; in recurseIntoAttrs ({
-    packages.${system} = import ./default.nix {
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+  in pkgs.recurseIntoAttrs ({
+    packages.${system} = import ./default.nix { inherit pkgs; };
   });
 }

4f1b30e5a9b1d70c220df6c4c1fc20634d6e5c59
SamLukeYes <samlukeyes123@gmail.com>
2023-02-18 19:49:37 +0800

    lx-music-desktop: 2.1.0 -> 2.1.1

--- default.nix
+++ default.nix
@@ -20,13 +20,13 @@ in {
   
   lx-music-desktop = callPackage ./electronAppImage rec {
     pname = "lx-music-desktop";
-    version = "2.1.0";
+    version = "2.1.1";
     description = "A music application based on electron";
     homepage = "https://lxmusic.toside.cn";
     license = lib.licenses.asl20;
     src = fetchurl {
       url = "${rp}https://github.com/lyswhut/${pname}/releases/download/v${version}/${pname}-v${version}-x64.AppImage";
-      hash = "sha256-PVV9EYY+/jw+QwqPqxfqKvo/BDP/y1kDVSlyOYywzss=";
+      hash = "sha256-fFpjuE7jRxjcFM+b80rTAMvAOEGIMdI4U0Uzqlo4dZ8=";
     };
   };
 

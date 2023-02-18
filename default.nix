e2bdd8f134c7e594b1a01da7d68f0d51136eedfe
SamLukeYes <samlukeyes123@gmail.com>
2023-02-18 20:08:57 +0800

    ppet: remove

--- default.nix
+++ default.nix
@@ -5,15 +5,7 @@
 
 with pkgs;
 
-let 
-  electron_19_1 = electron.overrideAttrs (old: rec {
-    version = "19.1.9";
-    src = fetchurl {
-      url = "${rp}https://github.com/electron/electron/releases/download/v${version}/electron-v${version}-linux-x64.zip";
-      hash = "sha256-/TIGdfFkfgPZZ2SpBsUcVnvwvL4DAVUORVnWbddnlt8=";
-    };
-  });
-in {
+{
   archlinux = recurseIntoAttrs (import ./archlinux {
     inherit pkgs rp;
   });
@@ -33,17 +25,4 @@ in {
   nodePackages = recurseIntoAttrs (import ./nodePackages/override.nix {
     inherit pkgs;
   });
-
-  ppet = callPackage ./electronAppImage rec {
-    electron = electron_19_1;
-    pname = "ppet3";
-    version = "3.3.0";
-    description = "Live2D on desktop";
-    homepage = "https://github.com/zenghongtu/PPet";
-    license = lib.licenses.mit;
-    src = fetchurl {
-      url = "${rp}${homepage}/releases/download/v${version}/PPet3-${version}.AppImage";
-      hash = "sha256-zdRZm0T6tBqNxvaUaiWAlAfuug/CQX0S+B5uDNdiQ/s=";
-    };
-  };
 }
